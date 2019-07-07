0bc0cc6401205fe56c9e2f2d95a69aa4bca81861
Amir <amirasyraf@outlook.com>
2019-07-07 16:31:56 +0800

    Initial Commit

---
+++ ACR128/ACR128.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ACR128
+{
+    class ACR120U
+    {
+        //=============================== Error Code ===============================
+
+        public const int ERR_ACR120_INTERNAL_UNEXPECTED = -1000;
+        public const int ERR_ACR120_PORT_INVALID = -2000;
+        public const int ERR_ACR120_PORT_OCCUPIED = -2010;
+        public const int ERR_ACR120_HANDLE_INVALID = -2020;
+        public const int ERR_ACR120_INCORRECT_PARAM = -2030;
+        public const int ERR_ACR120_READER_NO_TAG = -3000;
+        public const int ERR_ACR120_READER_READ_FAIL_AFTER_OP = -3010;
+        public const int ERR_ACR120_READER_NO_VALUE_BLOCK = -3020;
+        public const int ERR_ACR120_READER_OP_FAILURE = -3030;
+        public const int ERR_ACR120_READER_UNKNOWN = -3040;
+        public const int ERR_ACR120_READER_LOGIN_INVALID_STORED_KEY_FORMAT = -4010;
+        public const int ERR_ACR120_READER_WRITE_READ_AFTER_WRITE_ERROR = -4020;
+        public const int ERR_ACR120_READER_DEC_FAILURE_EMPTY = -4030;
+        public const int ERR_READER_VALUE_INC_OVERFLOW = -4031;
+        public const int ERR_READER_VALUE_OP_FAILURE = -4032;
+        public const int ERR_READER_VALUE_INVALID_BLOCK = -4033;
+        public const int ERR_READER_VALUE_ACCESS_FAILURE = -4034;
+        public const int ERR_READER_NO_RESPONSE = -5000;
+
+        //======================= Reader Port for AC_Open ==========================
+
+        public const int ACR120_USB1 = 0;
+        public const int ACR120_USB2 = 1;
+        public const int ACR120_USB3 = 2;
+        public const int ACR120_USB4 = 3;
+        public const int ACR120_USB5 = 4;
+        public const int ACR120_USB6 = 5;
+        public const int ACR120_USB7 = 6;
+        public const int ACR120_USB8 = 7;
+
+        //======================== Key Type for AC_Login ===========================
+
+        public const int ACR120_LOGIN_KEYTYPE_A = (int)(0xAA);
+
+        public const int ACR120_LOGIN_KEYTYPE_B = (int)(0xBB);
+        public const int ACR120_LOGIN_KEYTYPE_DEFAULT_A = (int)(0xAD);
+        public const int ACR120_LOGIN_KEYTYPE_DEFAULT_B = (int)(0xBD);
+        public const int ACR120_LOGIN_KEYTYPE_DEFAULT_F = (int)(0xFD);
+        public const int ACR120_LOGIN_KEYTYPE_STORED_A = (int)(0xAF);
+        public const int ACR120_LOGIN_KEYTYPE_STORED_B = (int)(0xBF);
+
+        //======================== Key Type for AC_Login ===========================
+        [StructLayout(LayoutKind.Sequential)]
+        public struct tReaderStatus
+        {
+            public byte MifareInterfaceType;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public byte[] CardsSupported;
+            public byte CardOpMode;
+            public byte FWI;		// the current FWI value (time out value)
+            public byte RFU;		// To be defined
+            public int RFU2;		// To be defined
+        };
+
+        //======================================================================================
+        // READER COMMANDS
+        //======================================================================================
+
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Open(int ReaderPort);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Close(int hReader);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Reset(int hReader);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Status(int hReader, ref byte pFirmwareVersion, ref tReaderStatus pReaderStatus);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_ReadRC531Reg(int hReader, byte RegNo, ref byte pRegData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_WriteRC531Reg(int hReader, byte RegNo, byte pRegData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_DirectSend(int hReader, byte DataLength, ref byte pData,
+                                                   ref byte pResponseDataLength, ref byte pResponseData, int TimedOut);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_DirectReceive(int hReader, byte RespectedDataLength, ref byte pReceivedDataLength,
+                                                      ref byte pReceivedData, int TimedOut);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_RequestDLLVersion(ref byte pVersionInfoLen, ref byte pVersionInfo);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_ReadEEPROM(int hReader, byte RegNo, ref byte pEEPROMData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_WriteEEPROM(int hReader, byte RegNo, byte eePROMData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_ReadUserPort(int hReader, ref byte pUserPortState);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_WriteUserPort(int hReader, byte userPortState);
+
+        //======================================================================================
+        // CARD COMMANDS
+        //======================================================================================
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Select(int hReader, ref byte pResultTagType,
+                                               ref byte pResultTagLength, ref byte pResultSN);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Login(int hReader, byte Sector, int keyType,
+                                              byte storedNo, ref byte pKey);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Read(int hReader, byte Block, ref byte pBlockData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_ReadValue(int hReader, byte Block, ref byte pValueData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Write(int hReader, byte Block, ref byte pBlockData);
+
+        [DllImport("ACR120U.DLL", CharSet = CharSet.Auto)]
+        public static extern int ACR120_WriteValue(int hReader, byte Block, int ValueData);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_WriteMasterKey(int hReader, byte keyNo, ref byte pKey);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Inc(int hReader, byte Block,
+                                            int value, ref int pNewValue);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Dec(int hReader, byte Block,
+                                            int value, ref int pNewValue);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Copy(int hReader, byte srcBlock,
+                                             byte desBlock, ref int pNewValue);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_Power(int hReader, byte State);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_ListTags(int hReader, ref byte pNumTagFound, ref byte pTagType,
+                                                 ref byte pTagLength, ref byte pSN);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_MultiTagSelect(int hReader, byte TagLength, ref byte SN,
+                                                       ref byte pResultTagType, ref byte pResultTagLength,
+                                                       ref byte pResultSN);
+
+        [DllImport("ACR120U.DLL")]
+        public static extern int ACR120_TxDataTelegram(int hReader, byte SendDataLength, ref byte pSendData,
+                                                       ref byte pReceivedDataLength, ref byte pReceivedData);
+
+        public static string GetErrMsg(int code)
+        {
+            switch (code)
+            {
+                case -1000:
+                    return ("Unexpected Internal Library Error : -1000");
+
+                case -2000:
+                    return ("Invalid Port : -2000");
+
+                case -2010:
+                    return ("Port Occupied by Another Application : -2010");
+
+                case -2020:
+                    return ("Invalid Handle : -2020");
+
+                case -2030:
+                    return ("Incorrect Parameter : -2030");
+
+                case -3000:
+                    return ("No TAG Selected or in Reachable Range : -3000");
+
+                case -3010:
+                    return ("Read Failed after Operation : -3010");
+
+                case -3020:
+                    return ("Block doesn't contain value : -3020");
+
+                case -3030:
+                    return ("Operation Failed : -3030");
+
+                case -3040:
+                    return ("Unknown Reader Error : -3040");
+
+                case -4010:
+                    return ("Invalid stored key format in login process : -4010");
+
+                case -4020:
+                    return ("Reader can't read after write operation : -4020");
+
+                case -4030:
+                    return ("Decrement Failure (Empty) : -4030");
+
+            }
+            return ("Error is not documented.");
+        }
+
+        public ACR120U()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
---
+++ ACR128/Program.cs
@@ -0,0 +1,228 @@
+﻿/*
+    ACR1281U-C8 Mifare Classic Serial Number Reader
+    Modularsoft Sdn Bhd
+    V1.0
+
+    Date Created        :       03/07/2019
+    Developer           :       Amir Asyraf | amirasyraf@outlook.com | 0193902459 | amirasyraf.dev
+    Description         :       Continually reads Mifare Classic RFID card and sends the serial number as input (like copy/paste)
+
+    TODOS               :       1) GUI
+                                2) Error Handling
+                                3) Clean Code
+ */
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+
+namespace ACR128
+{
+    public class ProgramConfig
+    {
+        public int organizationChoice { get; set; }
+        public string pollingRate { get; set; }
+    }
+    class Program
+    {
+        /* CONSTANTS */
+        private const int PORT = 0; // Always 0
+        private const int ASCC = 1;
+        private const int UTEM = 2;
+        private const int UPSI = 3;
+        private const int UUM = 4;
+        private const int DELAY_RATE = 1500; // Room for 'optimization' hehehehe. Used in Program_Delay()
+
+        private static int hReader = 0;
+        private static bool ISCONNECTED = false;
+        private static int status = -1; // Status code returned by ACR128
+
+        static ProgramConfig config = new ProgramConfig();
+
+        static void Main(string[] args)
+        {
+            string serialNumber = "0000000000";
+            string serialNumber2 = "0000000001";
+            byte[] dataRead = new byte[8]; // Will contain card's Serial Number AKA UID
+            byte[] tagType = new byte[50];
+            byte ResultTag = 0;
+
+            if (!Program_Initialize()) // Program_Initialize returns False. Exit program. Possible permission issue.
+                Exit_Program();
+
+            Reader_Initialize();
+
+            Program_Delay();
+
+            for (; ; )
+            {
+                /* Slow down reader read operation based on polling rate since there's no reason to have it run fast continually */
+                Thread.Sleep(Int32.Parse(config.pollingRate));
+
+                status = ACR120U.ACR120_Select(hReader, ref tagType[0], ref ResultTag, ref dataRead[0]); // Read operation
+
+                if (status == 0) // 0 = No error/successful
+                {
+                    serialNumber = "0000000000";
+                    serialNumber = ByteArrayToString(dataRead).Substring(0, 8);
+
+                    if (config.organizationChoice == UTEM) // UTEM uses numbers only. Convert from Hex to Int
+                        serialNumber = Convert.ToString(Int64.Parse(serialNumber, System.Globalization.NumberStyles.HexNumber));
+
+                    Console.WriteLine("Serial Number: " + serialNumber);
+
+                    /* Checks if the card being read is the same. If yes, no need to continually send input */
+                    if (string.Equals(serialNumber2, serialNumber))
+                        continue;
+                    else
+                    {
+                        SendKeys.SendWait(serialNumber.Trim());
+                        serialNumber2 = serialNumber;
+                    }
+
+                }
+
+                /* No card is detected by the reader. Sets both serial number variables to 00000000 as a reset procedure */
+                else if (status == 62536)
+                {
+                    serialNumber = "0000000000";
+                    serialNumber2 = "0000000000";
+                    Console.WriteLine("Serial Number: " + serialNumber);
+                    continue;
+                }
+                /* TODO: Error handling? */
+                else
+                    continue;
+            }
+        }
+        private static void Reader_Initialize()
+        {
+            Console.WriteLine("Connecting to reader...");
+            Reader_Disconnect();
+            Reader_Connect();
+
+            if (ISCONNECTED)
+                Console.WriteLine("Successful!");
+        }
+        private static void Reader_Connect()
+        {
+            while (hReader < 0)
+            {
+                hReader = ACR120U.ACR120_Open(PORT);
+
+                //if success return 0 else return < 0
+                if (hReader >= 0)
+                {
+                    if (!ISCONNECTED)
+                    {
+                        ISCONNECTED = true;
+                        break;
+                    }
+                    else
+                    {
+                    }
+                }
+                else
+                {
+                    string err = ACR120U.GetErrMsg(hReader);
+                    ISCONNECTED = false;
+                }
+            }
+        }
+        private static void Reader_Disconnect()
+        {
+            status = ACR120U.ACR120_Close(hReader);
+            hReader = -1;
+            ISCONNECTED = false;
+        }
+        private static string ByteArrayToString(byte[] data)
+        {
+            StringBuilder sDataOut;
+
+            if (data != null)
+            {
+                sDataOut = new StringBuilder(data.Length * 2);
+                for (int nI = 0; nI < data.Length; nI++)
+                    sDataOut.AppendFormat("{0:X02}", data[nI]);
+            }
+            else
+                sDataOut = new StringBuilder();
+
+            return sDataOut.ToString();
+        }
+        /* TODO: Error Handling for read operation of config.json */
+        private static Boolean Program_Initialize()
+        {
+            for (int fail = 0; fail < 3; fail++)
+            {
+                if (File.Exists("config.json"))
+                {
+                    Console.WriteLine("Reading Config File...");
+                    Program_Delay();
+                    config = JsonConvert.DeserializeObject<ProgramConfig>(File.ReadAllText(@"config.json"));
+                    Console.WriteLine("Successful!");
+                    break;
+                }
+                else // If file not found, assume as first initialization, create new config file
+                {
+                    Console.WriteLine("config.json not found. Initiating config initialization...");
+                    Program_Delay();
+                    Write_Config();
+                    Console.WriteLine("Writing config.json...");
+                    Program_Delay();
+                    Console.WriteLine("Successful!");
+                }
+
+                if (fail == 2)
+                    return false;
+            }
+
+            return true;
+        }
+        /* TODO: Error Handling for write operation of config.json and user input */
+        private static void Write_Config()
+        {
+            int choice;
+            string rate = String.Empty;
+            Console.WriteLine("\n\n****************PROGRAM INITIALIZATION****************\n");
+            Console.WriteLine("ASCC: 1");
+            Console.WriteLine("UTEM: 2");
+            Console.WriteLine("UPSI: 3");
+            Console.WriteLine("UUM: 4");
+            Console.WriteLine("Others: 5");
+            Console.Write("Enter organization: ");
+            choice = Convert.ToInt32(Console.ReadKey().Key.ToString().Substring(1));
+            Console.WriteLine();
+
+            Console.Write("Enter polling rate(ms)(recommended 250): ");
+            rate = Console.ReadLine();
+            Console.WriteLine();
+            if (rate == "")
+                rate = "250";
+
+            ProgramConfig config = new ProgramConfig
+            {
+                organizationChoice = choice,
+                pollingRate = rate
+            };
+            File.WriteAllText(@"config.json", JsonConvert.SerializeObject(config));
+        }
+        private static void Exit_Program()
+        {
+            Console.WriteLine("Unable to initialize program for first time use. Possible file permission issue.");
+            Console.WriteLine("Exiting Program, Press Any Key to Exit");
+            Console.ReadKey();
+            System.Environment.Exit(1);
+        }
+        private static void Program_Delay()
+        {
+            System.Threading.Thread.Sleep(DELAY_RATE);
+        }
+    }
+}
